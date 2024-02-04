f5e338036a0dfe90e35c150e9482fad2221f0b42
jongjin1010 <jongjin024@gmail.com>
2024-02-05 01:35:08 +0900

    add: default hook

---
+++ hooks/useInputs.ts
@@ -0,0 +1,17 @@
+import { useState, useCallback } from "react";
+
+function useInputs(initialForm: any) {
+  const [form, setForm] = useState(initialForm);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((form: any) => ({ ...form, [name]: value }));
+  }, []);
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  const setNull = useCallback((key: string) => {
+    setForm((form: any) => ({ ...form, [key]: "" }));
+  }, []);
+  return [form, onChange, reset, setNull];
+}
+
+export default useInputs;
