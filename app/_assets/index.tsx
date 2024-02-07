02d3b31d1e19d57e1c74469d7ccef8934c0eae74
jongjin1010 <jongjin024@gmail.com>
2024-02-07 23:15:45 +0900

    add: svg for header ui

---
+++ app/_assets/index.tsx
@@ -0,0 +1,9 @@
+export { HeaderLogo } from "./svg/HeaderLogo";
+export { HeaderSearchIcon, InputSearchIcon } from "./svg/SearchIcons";
+export { HomeIcon } from "./svg/HomeIcon";
+export { MakeGroupIcon } from "./svg/MakeGroupIcon";
+export {
+  DefaultProfileImage,
+  MyPageDefaultProfileImage,
+  ChattingDefaultProfileImage,
+} from "./svg/DefaultProfileImages";
---
+++ app/_assets/svg/MakeGroupIcon.tsx
@@ -0,0 +1,30 @@
+export const MakeGroupIcon = () => {
+  return (
+    <svg
+      style={{ cursor: "pointer" }}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="2"
+        y="2"
+        width="20"
+        height="20"
+        rx="4"
+        stroke="#B9B9B9"
+        strokeWidth="2"
+      />
+      <path
+        d="M10.8 7.2C10.8 6.53726 11.3373 6 12 6C12.6627 6 13.2 6.53726 13.2 7.2V16.8C13.2 17.4627 12.6627 18 12 18C11.3373 18 10.8 17.4627 10.8 16.8V7.2Z"
+        fill="#B9B9B9"
+      />
+      <path
+        d="M16.8 10.8C17.4627 10.8 18 11.3373 18 12C18 12.6627 17.4627 13.2 16.8 13.2H7.2C6.53726 13.2 6 12.6627 6 12C6 11.3373 6.53726 10.8 7.2 10.8H16.8Z"
+        fill="#B9B9B9"
+      />
+    </svg>
+  );
+};
