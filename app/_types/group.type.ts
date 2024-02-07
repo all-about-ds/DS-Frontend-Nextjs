9758cda234aa270e1be4e3de4e65cd2c718ec7fe
jongjin1010 <jongjin024@gmail.com>
2024-02-08 00:40:51 +0900

    add: group type

---
+++ app/_types/group.type.ts
@@ -0,0 +1,11 @@
+export type GroupType = {
+  idx: number;
+  name: string;
+  img: string;
+  description: string;
+  memberCount: number;
+  maxCount: number;
+  leaderImg: string;
+  leaderName: string;
+  secret: boolean;
+};
