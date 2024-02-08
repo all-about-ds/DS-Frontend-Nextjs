f14a3a80cd7e8ae097ca7f6e1bdc5f8254e5394e
jongjin1010 <jongjin024@gmail.com>
2024-02-09 01:34:47 +0900

    add: AppleSDGothicNeo font

--- app/(route)/global-style.tsx
+++ app/(route)/global-style.tsx
@@ -14,10 +14,29 @@ export default function GlobalStyle() {
         }
 
         @font-face {
+          font-family: "AppleSDGothicNeoB00";
+          font-style: normal;
+          src: url("/font/AppleSDGothicNeoB.woff") format("woff"),
+            url("/font/AppleSDGothicNeoB.woff2") format("font-woff2");
+        }
+
+        @font-face {
           font-family: "AppleSDGothicNeoM00";
           font-style: normal;
           src: url("/font/AppleSDGothicNeoM.woff") format("woff"),
             url("/font/AppleSDGothicNeoM.woff2") format("font-woff2");
+        }
+
+        @font-face {
+          font-family: "AppleSDGothicNeoEB00";
+          font-style: normal;
+          src: url("/font/AppleSDGothicNeoEB.woff") format("woff");
+        }
+
+        @font-face {
+          font-family: "AppleSDGothicNeoSB00";
+          font-style: normal;
+          src: url("/font/AppleSDGothicNeoSB.woff") format("woff");
         }
 
         body {
