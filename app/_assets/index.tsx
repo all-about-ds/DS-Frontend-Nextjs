a7112b91fe99321d4f7f98026208df1824fff5b2
jongjin1010 <jongjin024@gmail.com>
2024-02-10 20:03:54 +0900

    add: ExitIcon svg

--- app/_assets/index.tsx
+++ app/_assets/index.tsx
@@ -5,6 +5,7 @@ export {
   HeaderSearchIcon,
   InputSearchIcon,
   LockIcon,
+  ExitIcon,
 } from "./svg/Icons";
 export {
   DefaultProfileImage,
--- app/_assets/svg/Icons.tsx
+++ app/_assets/svg/Icons.tsx
@@ -118,3 +118,36 @@ export const InputSearchIcon = () => {
     </svg>
   );
 };
+
+export const ExitIcon = () => {
+  return (
+    <>
+      <svg
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="21.6666"
+          y="6.66672"
+          width="2.35702"
+          height="21.2132"
+          rx="1"
+          transform="rotate(45 21.6666 6.66672)"
+          fill="#161616"
+        />
+        <rect
+          x="23.3334"
+          y="21.6666"
+          width="2.35702"
+          height="21.2132"
+          rx="1"
+          transform="rotate(135 23.3334 21.6666)"
+          fill="#161616"
+        />
+      </svg>
+    </>
+  );
+};
