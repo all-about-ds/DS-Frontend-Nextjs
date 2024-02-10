3d9b5b45683f9af2fd3587a313d055e9debb40b3
jongjin1010 <jongjin024@gmail.com>
2024-02-10 14:28:55 +0900

    update: props type

--- app/_components/ui/post-card/main/index.tsx
+++ app/_components/ui/post-card/main/index.tsx
@@ -2,13 +2,20 @@ import * as S from "./style";
 import * as Image from "@/app/_assets";
 import { GroupType } from "@/app/_types/group.type";
 
-export default function MainPostCard(props: GroupType) {
+type MainPostCardPropsType = {
+  cardProps: GroupType;
+};
+
+export default function MainPostCard(props: MainPostCardPropsType) {
   return (
     <>
-      <S.PostCard>
+      <S.GroupBox>
         <div style={{ position: "relative" }}>
-          <S.Thumbnail src={props.img} alt="그룹이미지"></S.Thumbnail>
-          {props.secret && (
+          <S.Thumbnail
+            src={props.cardProps?.img}
+            alt="그룹이미지"
+          ></S.Thumbnail>
+          {props.cardProps?.secret && (
             <S.StyledLockIcon>
               <Image.LockIcon />
             </S.StyledLockIcon>
@@ -16,23 +23,26 @@ export default function MainPostCard(props: GroupType) {
         </div>
         <S.TextBox>
           <S.MaxPeople>
-            현재 {props.memberCount}/{props.maxCount}명
+            현재 {props.cardProps?.memberCount}/{props.cardProps?.maxCount}명
           </S.MaxPeople>
-          <S.Title>{props.name}</S.Title>
-          <S.Description>{props.description}</S.Description>
+          <S.Title>{props.cardProps?.name}</S.Title>
+          <S.Description>{props.cardProps?.description}</S.Description>
         </S.TextBox>
         <S.User>
-          {!props.leaderImg && (
+          {!props.cardProps?.leaderImg && (
             <div style={{ marginRight: 8 }}>
               <Image.DefaultProfileImage />
             </div>
           )}
-          {props.leaderImg && (
-            <S.Profile src={props.leaderImg} alt="방장 프로필"></S.Profile>
+          {props.cardProps?.leaderImg && (
+            <S.Profile
+              src={props.cardProps?.leaderImg}
+              alt="방장 프로필"
+            ></S.Profile>
           )}
-          <S.UserName>{props.leaderName}</S.UserName>
+          <S.UserName>{props.cardProps?.leaderName}</S.UserName>
         </S.User>
-      </S.PostCard>
+      </S.GroupBox>
     </>
   );
 }
