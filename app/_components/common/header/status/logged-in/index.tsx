f2495659b289fd385fb85531c740b51faffdae34
jongjin1010 <jongjin024@gmail.com>
2024-02-10 20:05:03 +0900

    add: header features

--- app/_components/common/header/status/logged-in/index.tsx
+++ app/_components/common/header/status/logged-in/index.tsx
@@ -1,29 +1,97 @@
+import { SearchAtom, UserDataAtomFamily } from "@/app/_atoms";
+import { useRecoilState } from "recoil";
 import * as S from "./style";
 import * as Image from "@/app/_assets/index";
+import { useEffect, useState } from "react";
+import userRequest from "@/app/_api/request/user.request";
+import { useRouter } from "next/navigation";
 
 export default function LoggedInHeader() {
+  const router = useRouter();
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchValues, setSearchValues] = useRecoilState(SearchAtom);
+  const [userName, setUserName] = useRecoilState(UserDataAtomFamily("name"));
+  const [userImage, setUserImage] = useRecoilState(UserDataAtomFamily("image"));
+
+  const getUser = async () => {
+    try {
+      const response: any = await userRequest.getUserData();
+
+      setUserName(response.data.name);
+      setUserImage(response.data.img);
+    } catch (e) {
+      Promise.reject(e);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValues((oldValue) => ({
+      ...oldValue,
+      keyword: e.target.value,
+    }));
+  };
+
+  const onPressEnterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") {
+      if (!e.nativeEvent.isComposing) {
+        e.preventDefault();
+        setSearchValues((oldValue) => ({
+          ...oldValue,
+          isSearchRequested: true,
+        }));
+      }
+    }
+  };
+
   return (
     <>
-      <S.SearchBar>
-        <div style={{ marginTop: 4 }} className="input-search-icon">
-          <Image.InputSearchIcon />
-        </div>
-        <S.SearchArea placeholder="찾고 싶은 그룹의 이름을 입력해주세요."></S.SearchArea>
-      </S.SearchBar>
-      <S.HeaderContentBox isSearching={false}>
-        <div className="search-icon">
+      {isSearching && (
+        <S.SearchBar>
+          <div style={{ marginTop: 4 }} className="input-search-icon">
+            <Image.InputSearchIcon />
+          </div>
+          <S.SearchArea
+            placeholder="찾고 싶은 그룹의 이름을 입력해주세요."
+            onChange={onSearch}
+            value={searchValues.keyword}
+            onKeyDown={onPressEnterKey}
+            autoFocus={true}
+          ></S.SearchArea>
+        </S.SearchBar>
+      )}
+      <S.HeaderContentBox isSearching={isSearching}>
+        <div
+          className="search-icon"
+          onClick={() => setIsSearching(!isSearching)}
+        >
           <Image.HeaderSearchIcon />
         </div>
-        <div className="home-icon">
+        <div className="home-icon" onClick={() => router.replace("/")}>
           <Image.HomeIcon />
         </div>
-        <div>
+        <div onClick={() => router.replace("/make-group")}>
           <Image.MakeGroupIcon />
         </div>
-        <div style={{ cursor: "pointer" }}>
-          <Image.DefaultProfileImage />
-        </div>
-        <S.UserName>오종진님</S.UserName>
+        {!userImage && (
+          <div
+            onClick={() => router.push("/my-page")}
+            style={{ cursor: "pointer" }}
+          >
+            <Image.DefaultProfileImage />
+          </div>
+        )}
+        {userImage && (
+          <S.UserProfile
+            src={userImage}
+            alt="유저 프로필"
+            onClick={() => router.push("/my-page")}
+          />
+        )}
+        <S.UserName>{userName}</S.UserName>
       </S.HeaderContentBox>
     </>
   );
