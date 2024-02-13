6f34f745a0de08fe565a3fd95de9f4cca4594652
jongjin1010 <jongjin024@gmail.com>
2024-02-13 21:15:57 +0900

    add: signup page

---
+++ app/_components/page/auth/signup/style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const GoLoginBox = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: auto;
+  gap: 5px;
+  margin-top: 1.25rem;
+
+  p {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    color: #c2c2c2;
+  }
+`;
