fe14269be798beafc891dd126c4f54800cfcda14
jongjin1010 <jongjin024@gmail.com>
2024-02-13 16:06:26 +0900

    add: auth page types

--- app/_types/auth.type.ts
+++ app/_types/auth.type.ts
@@ -1,6 +1,30 @@
+import { SetterOrUpdater } from "recoil";
+
 export type TokenType = {
   accessToken: string;
   refreshToken: string;
   accessExp: string;
   refreshExp: string;
+};
+
+export type LoginType = {
+  email: string;
+  password: string;
+};
+
+export type SignupType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type FindPasswordType = {
+  email: string;
+  password: string;
+};
+
+export type AuthFormSectionPropsType = {
+  title: string;
+  setSection: SetterOrUpdater<number>;
+  atomKey: string;
 };
