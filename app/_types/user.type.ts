129d07b8a87d54c9067afdda0a8f7d8349dd571c
jongjin1010 <jongjin024@gmail.com>
2024-02-17 19:52:23 +0900

    update: change interface -> type

--- app/_types/user.type.ts
+++ app/_types/user.type.ts
@@ -1,22 +1,22 @@
-export interface GetMyDataType {
+export type GetMyDataType = {
   idx: number;
   name: string;
   profileImg: string;
   groups: MyGroupListType[];
-}
+};
 
-export interface MyGroupListType {
+export type MyGroupListType = {
   idx: number;
   name: string;
   img: string;
-}
+};
 
-export interface TimerUserType {
+export type TimerUserType = {
   name: string;
   time: number;
   active: boolean;
   id: number;
-}
+};
 
 export type MemberItemPropsType = {
   isClicked: boolean;
