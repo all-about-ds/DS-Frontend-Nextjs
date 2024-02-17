41523393b0bffd41f5da837df1abc63287a4785b
jongjin1010 <jongjin024@gmail.com>
2024-02-17 12:58:50 +0900

    update: group information page optimization

--- app/_components/page/group/information/index.tsx
+++ app/_components/page/group/information/index.tsx
@@ -4,21 +4,21 @@ import * as S from "./style";
 import * as Image from "@/app/_assets";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { ref, remove } from "@firebase/database";
-import { GroupInformationType } from "@/app/_types/group.type";
 import groupRequest from "@/app/_api/request/group.request";
 import { db } from "@/app/_shared/firebase";
 import { useRouter } from "next/navigation";
 import memberRequest from "@/app/_api/request/member.request";
 import GroupPageHeader from "@/app/_components/common/header/group-page";
+import DefaultModal from "@/app/_components/modal/default";
+import { GroupInformationType } from "@/app/_types/group.type";
 import {
+  GroupDataAtom,
   GroupIsClickedAtom,
   ModalAtomFamily,
   UserDataAtomFamily,
 } from "@/app/_atoms";
-import DefaultModal from "@/app/_components/modal/default";
-import Link from "next/link";
 
 export default function GroupInformationPage({ groupId }: { groupId: number }) {
   const [leaveGroupModal, setLeaveGroupModal] = useRecoilState(
@@ -27,39 +27,27 @@ export default function GroupInformationPage({ groupId }: { groupId: number }) {
   const [deleteGroupModal, setDeleteGroupModal] = useRecoilState(
     ModalAtomFamily("deleteGroup")
   );
-
   const [userName] = useRecoilState(UserDataAtomFamily("name"));
-  const [isOwner, setIsOwner] = useState<boolean>(false);
-  const [information, setInformation] = useState<GroupInformationType>();
+  const groupData = useRecoilValue(GroupDataAtom);
+  const [data, setData] = useState<GroupInformationType>();
   const [, setGroupIsClicked] = useRecoilState(GroupIsClickedAtom);
   const router = useRouter();
 
-  const memberList = JSON.stringify(information?.memberList);
-
   useEffect(() => {
-    const getGroupInformationById = async () => {
-      try {
-        const res: any = await groupRequest.getGroupInformation(
-          String(groupId)
-        );
-        setIsOwner(res.data.host);
-        setInformation(res.data);
-      } catch (e: any) {
-        if (e.response.status === 404) {
-          toast.error("존재하지 않는 그룹입니다");
-          router.replace("/");
-        }
+    const checkServerClientSync = () => {
+      if (groupData.name !== "") {
+        setData(groupData);
       }
     };
 
-    getGroupInformationById();
-  }, []);
+    checkServerClientSync();
+  }, [groupData]);
 
   const deleteGroup = async () => {
     try {
-      await groupRequest.deleteGroup(information?.idx);
-      remove(ref(db, `chattings/${information?.name}`));
-      remove(ref(db, `timers/${information?.name}`));
+      await groupRequest.deleteGroup(data?.idx);
+      remove(ref(db, `chattings/${data?.name}`));
+      remove(ref(db, `timers/${data?.name}`));
 
       setDeleteGroupModal(false);
       toast.error("삭제되었습니다!");
@@ -73,11 +61,11 @@ export default function GroupInformationPage({ groupId }: { groupId: number }) {
   const leaveGroup = async () => {
     try {
       await memberRequest.leaveGroup(String(groupId));
-      remove(ref(db, `timers/${information?.name}/users/${userName}`));
+      remove(ref(db, `timers/${data?.name}/users/${userName}`));
 
       setLeaveGroupModal(false);
       toast.success("그룹을 나갔어요");
-      router.replace("/my");
+      router.replace("/my-page");
     } catch {
       setLeaveGroupModal(false);
       toast.error("알 수 없는 오류에요");
@@ -104,30 +92,21 @@ export default function GroupInformationPage({ groupId }: { groupId: number }) {
           executeFunc={deleteGroup}
         />
       )}
-      <GroupPageHeader title={information?.name} groupId={groupId} />
-      <S.GroupImage src={information?.img} alt="그룹 이미지" />
+      <GroupPageHeader idx={groupId} />
+      <S.GroupImage src={data?.img} alt="그룹 이미지" />
       <S.TitleBox>
-        <S.Title>{information?.name}</S.Title>
-        {isOwner ? (
+        <S.Title>{data?.name}</S.Title>
+        {data?.host ? (
           <S.GroupManageButtonBox>
             <div onClick={() => setDeleteGroupModal(true)}>
               <Image.DeleteButton />
             </div>
-            <Link
-              href={{
-                pathname: "/group/edit",
-                query: {
-                  idx: information?.idx,
-                  img: information?.img,
-                  title: information?.name,
-                  description: information?.description,
-                  maxCount: information?.memberList.length,
-                },
-              }}
-              as={"/group/edit"}
+            <div
+              style={{ cursor: "pointer" }}
+              onClick={() => router.push("/group/edit")}
             >
               <Image.OwnerButton />
-            </Link>
+            </div>
           </S.GroupManageButtonBox>
         ) : (
           <S.LeaveGroupText onClick={() => setLeaveGroupModal(true)}>
@@ -135,20 +114,14 @@ export default function GroupInformationPage({ groupId }: { groupId: number }) {
           </S.LeaveGroupText>
         )}
       </S.TitleBox>
-      <S.Description>{information?.description}</S.Description>
+      <S.Description>{data?.description}</S.Description>
       <S.Line />
       <S.TextMembersBox>
         <S.TextMembers>그룹원들</S.TextMembers>
-        {isOwner && (
-          <Link
-            href={{
-              pathname: "/group/" + groupId + "/member",
-              query: { list: memberList },
-            }}
-            as={"/group/" + groupId + "/member"}
-          >
+        {data?.host && (
+          <div onClick={() => router.push("/group/" + data?.idx + "/member")}>
             <Image.OwnerButton />
-          </Link>
+          </div>
         )}
       </S.TextMembersBox>
       <div style={{ margin: "0px 0px 2px 0.3975rem" }}>
@@ -156,19 +129,19 @@ export default function GroupInformationPage({ groupId }: { groupId: number }) {
       </div>
       <S.MemberList>
         <S.MemberBox key={"head"}>
-          {!information?.head.profileImg && <Image.DefaultProfileImage />}
-          {information?.head.profileImg && (
+          {!data?.head.profileImg && <Image.DefaultProfileImage />}
+          {data?.head.profileImg && (
             <S.MemberImage
-              src={information?.head.profileImg}
+              src={groupData?.head.profileImg}
               alt="그룹원 이미지"
             />
           )}
           <div>
             <S.MemberRole>그룹 부장</S.MemberRole>
-            <S.MemberName>{information?.head.name}</S.MemberName>
+            <S.MemberName>{data?.head.name}</S.MemberName>
           </div>
         </S.MemberBox>
-        {information?.memberList.map((member) => (
+        {data?.memberList.map((member) => (
           <S.MemberBox key={member.idx}>
             {!member.profileImg && <Image.DefaultProfileImage />}
             {member.profileImg && (
