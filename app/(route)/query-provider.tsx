9e8664f6361ea2ae93fb2e59f41fdea822d82936
jongjin1010 <jongjin024@gmail.com>
2024-02-18 18:24:28 +0900

    add: query provider in root layout

---
+++ app/(route)/query-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
