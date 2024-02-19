ea69540ebe2822c0e60524842ed966e4dc927a0b
jongjin1010 <jongjin024@gmail.com>
2024-02-20 01:13:46 +0900

    add: group page metadata

--- app/(route)/group/(group-page-header)/[groupId]/information/page.tsx
+++ app/(route)/group/(group-page-header)/[groupId]/information/page.tsx
@@ -1,4 +1,14 @@
 import GroupInformationPage from "@/app/_components/page/group/information";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DS | 그룹정보",
+  description: "그룹의 정보를 한 눈에 살펴봐요.",
+  openGraph: {
+    title: "DS | 그룹정보",
+    description: "그룹의 정보를 한 눈에 살펴봐요.",
+  },
+};
 
 export default function GroupInformation({
   params,
--- app/(route)/group/(normal-page-header)/members-management/page.tsx
+++ app/(route)/group/(normal-page-header)/members-management/page.tsx
@@ -1,4 +1,14 @@
 import GroupMembersManagementPage from "@/app/_components/page/group/members-management";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DS | 멤버관리",
+  description: "그룹원을 관리해요.",
+  openGraph: {
+    title: "DS | 멤버관리",
+    description: "그룹원을 관리해요.",
+  },
+};
 
 export default function GroupMembersManagement() {
   return <GroupMembersManagementPage />;
